27c62f3ec83216ddb60fb0828635aff6ac3bd967
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-06 10:42:02 -0600

    removing erroneous comments

--- Ionic-weather-app/www/js/app.js
+++ Ionic-weather-app/www/js/app.js
@@ -1,8 +1,4 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('weatherApp', ['ionic', 'ngCordova', 'angular-skycons'])
 
 .run(function($ionicPlatform) {
@@ -19,17 +15,12 @@ angular.module('weatherApp', ['ionic', 'ngCordova', 'angular-skycons'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-
-  // setup an abstract state for the tabs directive
     .state('main', {
     url: '/main',
     templateUrl: 'templates/main.html'
     })
 
-    // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('main');
 
 
